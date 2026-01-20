ad943af411c19b2b59428f8cbc56eee97fbb1dde
Amaank-git <amaank@eng.rizvi.edu.in>
2026-01-20 10:13:29 +0530

    Add files via upload

---
+++ exp 1.py
@@ -0,0 +1,8 @@
+name=input("enter your name:")
+age=input("enter your age:")
+address=input("enter your address:")
+print("hello!",name)
+print("how are you?",name)
+print(f"hello!{name}")
+print(f"I am {age} years old")
+print(f"I live at {address}")S
