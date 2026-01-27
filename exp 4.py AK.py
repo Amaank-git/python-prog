66055e37ea3d16c97c181473cdfc9e3f0b9be91c
Amaank-git <amaank@eng.rizvi.edu.in>
2026-01-27 10:14:10 +0530

    Add files via upload

---
+++ exp 4.py AK.py
@@ -0,0 +1,6 @@
+
+principle_amount = float(input("enter the principle amount :"))
+rate_of_interest = float(input("enter the rate of interest in % per year:"))
+time = float(input("enter time in years:"))
+si = (principle_ amount*rate_of_interest*time)/2
+print(f"simple interest = (si) ")
