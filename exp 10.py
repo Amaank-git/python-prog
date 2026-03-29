f9957e5396bdfbdb2670593aeaa9f0de641bb191
Amaank-git <amaank@eng.rizvi.edu.in>
2026-03-29 13:15:09 +0530

    Add files via upload

---
+++ exp 10.py
@@ -0,0 +1,14 @@
+#FACTORIAL GENERATOR
+print("UIN : 251A002 | DATE : 10-02-2026")
+n = int(input("ENTER NUMBER : "))
+flag = False
+for i in range(2, n):
+  if n%i == 0:
+    Flag = True
+    break
+  else:
+    Flag = False
+if Flag == True:
+  print("NOT PRIME")
+else:
+  print("PRIME")
